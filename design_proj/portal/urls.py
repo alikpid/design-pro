f583d194622576e92e19ca48643853feb0c54a80
AlinaK <koromislo.molodec@gmail.com>
2022-10-22 03:32:40 +0700

    авторизация

--- design_proj/portal/urls.py
+++ design_proj/portal/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path
-from .views import profile
 from . import views
+from .views import BBLoginView
+from .views import profile
 
 app_name = 'portal'
 
 
 urlpatterns = [
     path('accounts/profile/', profile, name='profile'),
-    path('accounts/login', views.BBLoginView, name='login'),
+    path('accounts/login/', BBLoginView.as_view(), name='login'),
     path('', views.index, name='index'),
 ]
+
