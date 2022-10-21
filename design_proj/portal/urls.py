7a4a687bfb0a63bcedc31b321de8d1e3f404864d
AlinaK <koromislo.molodec@gmail.com>
2022-10-22 01:00:43 +0700

    я щас убьюсь

--- design_proj/portal/urls.py
+++ design_proj/portal/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
+from .views import profile
 from . import views
+
+app_name = 'portal'
 
 
 urlpatterns = [
+    path('accounts/profile/', profile, name='profile'),
+    path('accounts/login', views.BBLoginView, name='login'),
     path('', views.index, name='index'),
 ]
--- design_proj/portal/views.py
+++ design_proj/portal/views.py
@@ -1,5 +1,27 @@
 from django.shortcuts import render
+from django.contrib.auth.views import LoginView
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse, Http404
+from django.template import TemplateDoesNotExist
+from django.template.loader import get_template
 
 
 def index(request):
     return render(request, 'index.html')
+
+
+def other_page(request, page):
+    try:
+        template = get_template('portal/' + page + '.html')
+    except TemplateDoesNotExist:
+        raise Http404
+    return HttpResponse(template.render(request=request))
+
+
+class BBLoginView(LoginView):
+    template_name = 'portal/login.html'
+
+
+@login_required
+def profile(request):
+    return render(request, 'portal/profile.html')
