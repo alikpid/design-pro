801c2009e183943bba616ce2311899472929df97
AlinaK <koromislo.molodec@gmail.com>
2022-10-24 05:49:14 +0700

    регистрация которая сохранаяет пароль юзера

--- design_proj/users/forms.py
+++ design_proj/users/forms.py
@@ -33,6 +33,15 @@ class RegisterUserForm(forms.ModelForm):
             )}
             raise ValidationError(errors)
 
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data['password1'])
+        user.is_active = True
+        user.is_activated = True
+        if commit:
+            user.save()
+        return user
+
     class Meta:
         model = get_user_model()
         fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2', 'user_agreement')
